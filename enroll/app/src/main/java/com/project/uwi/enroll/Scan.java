fd4e41a62862d5848f6b976a61fe0ae054a9c1a2
Mitra Kalloo <Mkalloo42@gmail.com>
2016-04-20 13:03:30 -0400

    core features

--- enroll/app/src/main/java/com/project/uwi/enroll/Scan.java
+++ enroll/app/src/main/java/com/project/uwi/enroll/Scan.java
@@ -10,21 +10,28 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.firebase.client.ValueEventListener;
 import com.project.uwi.enroll.com.google.zxing.integration.android.IntentIntegrator;
 import com.project.uwi.enroll.com.google.zxing.integration.android.IntentResult;
 
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class Scan extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageView scanBtn;
+    private Button scanBtn;
     private TextView txt_courseCode, StudentID;
-    private String courseCode="Code";
+    private String courseCode = "Code";
     private Map<String, Integer> map = new HashMap<>();
     ImageView img_present;
-    Firebase mRootRef;
+    Firebase mRootRef, mdate;
+    String Date, DATE = "";
 
 
     @Override
@@ -37,16 +44,35 @@ public class Scan extends AppCompatActivity implements View.OnClickListener {
 
 
         Bundle bundle = getIntent().getExtras();
-        if (bundle.containsKey("courseCode")){
+        if (bundle.containsKey("courseCode")) {
             courseCode = bundle.getString("courseCode");
             mRootRef = new Firebase("https://enroll.firebaseio.com/");
-
         }
+        if (bundle.containsKey("Date")) {
+            Date = bundle.getString("Date");
+        }
+
+
+        mdate = mRootRef.child("TIME").child(courseCode);
+
+        mdate.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                DATE = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+
         setUpUi();
     }
 
-    public void setUpUi(){
-        scanBtn = (ImageView) findViewById(R.id.scan_button);
+    public void setUpUi() {
+        scanBtn = (Button) findViewById(R.id.scan_button);
         map.put("scan_success", R.drawable.present);
         img_present = (ImageView) findViewById(R.id.img_scan);
         StudentID = (TextView) findViewById(R.id.Student_ID);
@@ -73,13 +99,32 @@ public class Scan extends AppCompatActivity implements View.OnClickListener {
             String scanContent = scanningResult.getContents();
             StudentID.setText("ID: " + scanContent);
             img_present.setImageResource(map.get("scan_success"));
-            Firebase course = mRootRef.child(courseCode);
+
+            long date = System.currentTimeMillis();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            String dateString = sdf.format(date);
+
+            Firebase course = mRootRef.child(courseCode + "/" + dateString);
+            Firebase temp = mRootRef.child(courseCode + "date");
+
+            if (DATE == null){
+                temp.push().setValue(dateString);
+                mdate.setValue(dateString);
+            }
+            else if(!DATE.equalsIgnoreCase(dateString)) {
+                temp.push().setValue(dateString);
+                mdate.setValue(dateString);
+            }
+
+
             course.push().setValue(scanContent);
-        }else{
+
+        } else {
             Toast toast = Toast.makeText(getApplicationContext(),
                     "No scan data received!", Toast.LENGTH_SHORT);
             toast.show();
         }
     }
+
 
 }
